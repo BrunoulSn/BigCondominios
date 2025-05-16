970c9148f3d389303b489be43e61e7a80e5caa9c
Bruno Cristiano <bruno120505@gmail.com>
2025-05-15 21:15:44 -0300

    Atualizando

--- Back-End/src/main/java/com/api/BlogAppApi/DTOs/BlogAppRecordDto.java
+++
@@ -1,6 +0,0 @@
-package com.api.BlogAppApi.DTOs;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record BlogAppRecordDto(@NotBlank String autor, @NotBlank String titulo, @NotBlank String texto) {
-}
